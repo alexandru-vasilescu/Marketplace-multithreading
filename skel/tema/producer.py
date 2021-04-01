35e4efacd993580c2a48a122a385d617ceb23c1e
Alexandru Vasilescu <madalin@DESKTOP-PPMRTBL.localdomain>
2021-04-01 17:57:54 +0300

    create repository

---
+++ skel/tema/producer.py
@@ -0,0 +1,43 @@
+"""
+This module represents the Producer.
+
+Computer Systems Architecture Course
+Assignment 1
+March 2021
+"""
+
+from threading import Thread
+
+
+class Producer(Thread):
+    """
+    Class that represents a producer.
+    """
+
+    def __init__(self, products, marketplace, republish_wait_time, **kwargs):
+        """
+        Constructor.
+
+        @type products: List()
+        @param products: a list of products that the producer will produce
+
+        @type marketplace: Marketplace
+        @param marketplace: a reference to the marketplace
+
+        @type republish_wait_time: Time
+        @param republish_wait_time: the number of seconds that a producer must
+        wait until the marketplace becomes available
+
+        @type kwargs:
+        @param kwargs: other arguments that are passed to the Thread's __init__()
+        """
+        Thread.__init__(self, daemon=True)
+        self.products = products
+        self.marketplace = marketplace
+        self.wait_time = republish_wait_time
+        for k, v in kwargs.items():
+            if k == 'name':
+                self.name = v
+
+    def run(self):
+        pass
